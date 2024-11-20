699f84eba70c34065df0a6ad8d28afe691001465
Attique Zaheer <mattiquezaheer@gmail.com>
2024-11-20 18:05:01 +0500

    dashboard-container_sidebar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,21 +1,26 @@
+"use client";
 import Sidebar from "../_components/Sidebar/Sidebar";
+import { changeState } from "../store/slices/sidebar";
+import { useAppDispatch, useAppSelector } from "@/app/store/hook";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+    const collapsed = useAppSelector((state) => state.sidebar.collapsed);
+    // const dispatch = useAppDispatch();
     return (
         <html lang="en">
             <head>
                 <title>Dashboard</title>
                 <meta name="description" content="Dashboard page of the application" />
             </head>
-            <>
+            <body>
                 <div className="flex">
-                    <div className="">
+                    <div>
                         <Sidebar />
                     </div>
-                    <div className="">{children}</div>
-                    
+                    {/* <div>{children}</div> */}
+                    <main className={`min-h-screen flex-grow bg-red-200 transition-all duration-300 ${collapsed ? "ml-16" : "ml-64"}`}>{children}</main>
                 </div>
-            </>
+            </body>
         </html>
     );
 }
